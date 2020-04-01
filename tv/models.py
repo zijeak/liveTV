664b589ccc97338050ac342ea80063aad6c41218
zijeak <51364235+zijeak@users.noreply.github.com>
2020-04-01 22:20:34 +0800

    Update models.py

--- tv/models.py
+++ tv/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class channel(models.Model):
     name = models.CharField(max_length=200,verbose_name='频道名称',null=False)
-    address = models.CharField(max_length=200,verbose_name='频道串流',null=False)
+    address = models.CharField(max_length=1000,verbose_name='频道串流',null=False)
 
     category = models.ForeignKey('Category')
 
